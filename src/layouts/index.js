b583509a3cf11fff5eea3e8f92c09af94e6ec6d5
Soreine <soreine.plume@gmail.com>
2018-04-14 09:45:12 +0200

    Delete header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/header'
 import './index.css'
 
 const Layout = ({ children, data }) => (
@@ -10,19 +9,11 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Modern web publication of the free version of the book Mindfulness in Plain English' },
+        { name: 'keywords', content: 'mindfulness, plain, english, meditation, vipassana, free' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
